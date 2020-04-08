ae8fe81a34b3093aad24414e744de7f833fa3c3e
bayramguney <bayramguney@yahoo.com>
2020-04-07 21:10:41 -0400

    initial

---
+++ src/test/java/Exercise.java
@@ -0,0 +1,36 @@
+ public class Exercise {
+
+    // 1--kendinden once gelen harf alfabetik olarak sonra geliyorsa o harfi almiyor-------------------------------
+    public static String  alphabetical(String str) {
+
+        String st = " " + str.charAt(0);
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i + 1) > str.charAt(i)) {
+                st = st + str.charAt(i + 1);
+            }
+        }
+        return st;
+    }
+    // 2--klasik faktoriyel----------------------------------------------------------------------------------------------------
+    public static long factorial(int number) {
+        long num = 1;
+        for (int i = number; 1 <= i; i--) {
+            num *= i;
+        }
+        return num;
+    }
+    //3 Stringteki karakterleri atiyor----------------------------------------------------------------------------------------
+    public static String removeCharacter(String str){
+        str=str.replaceAll("[!,@,#,$,%,&,*]","");
+        return str;
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+}
+
---
+++ src/test/java/Function.java
@@ -0,0 +1,10 @@
+public class Function extends Exercise {
+
+    public static void main(String[] args){
+        System.out.println("alphabetical-----"+alphabetical("hello"));
+        System.out.println("factorial-----"+factorial(6));
+        System.out.println("alphabetical-----"+removeCharacter("sfdgtr$%hhfdjf&!tyu67"));
+
+    }
+//
+}
